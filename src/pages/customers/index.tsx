9e2ff895f440588fc75aaa624d41caf69170b1c1
Taha Ebrahimzadeh <taha13800111@gmail.com>
2023-09-21 14:07:17 +0330

    Add customers page

--- src/pages/customers/index.tsx
+++ src/pages/customers/index.tsx
@@ -1,8 +1,16 @@
 // module
 import { FC } from 'react'
+// custom
+import CustomersTable from './customers-table'
+import Customer from '../../models/entities/customer'
+
+const customers: Customer[] = [
+    { name: 'ppppppppp', phone: '222222222222222' },
+    { name: 'ssssssss', phone: '111111111' },
+]
 
 const Customers: FC = () => {
-    return null
+    return <CustomersTable rows={customers} />
 }
 
 export default Customers
