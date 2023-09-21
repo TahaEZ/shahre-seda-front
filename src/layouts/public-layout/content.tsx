97b308b8d286c97b433dfe6c0a734417860af96f
Taha Ebrahimzadeh <taha13800111@gmail.com>
2023-09-21 21:23:59 +0330

    Style public layout

---
+++ src/layouts/public-layout/content.tsx
@@ -0,0 +1,25 @@
+// module
+import { Box, styled } from "@mui/material";
+import { FC } from "react";
+
+interface ContentProps {
+    Cmp: FC
+}
+
+const Content: FC<ContentProps> = ({ Cmp }) => {
+    return (
+        <ContentWrapper>
+            <Cmp />
+        </ContentWrapper>
+    )
+}
+
+const ContentWrapper = styled(Box)(() => ({
+    alignItems: 'center',
+    display: 'flex',
+    height: '100%',
+    justifyContent: 'center',
+    padding: 0,
+}))
+
+export default Content
--- src/layouts/public-layout/index.tsx
+++ src/layouts/public-layout/index.tsx
@@ -1,6 +1,8 @@
 // module
 import { FC } from 'react'
 import { Box, styled } from '@mui/material'
+// custom
+import Content from './content'
 
 interface PublicLayoutProps {
     Cmp: FC
@@ -9,13 +11,14 @@ interface PublicLayoutProps {
 const PublicLayout: FC<PublicLayoutProps> = ({ Cmp }) => {
     return (
         <PublicLayoutWrapper>
-            <Cmp />
+            <Content Cmp={Cmp} />
         </PublicLayoutWrapper>
     )
 }
 
 const PublicLayoutWrapper = styled(Box)({
     boxSizing: 'border-box',
+    height: '100%',
     margin: 0,
     padding: 0,
     display: 'flex',
