9c0e7df14c1acfbf1e1702564e4b10d4002c9800
Taha Ebrahimzadeh <taha13800111@gmail.com>
2023-09-21 20:49:25 +0330

    Customize app theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,45 @@
 // module
+import { useMemo } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { CssBaseline, GlobalStyles, ThemeOptions, ThemeProvider, createTheme } from '@mui/material'
 // custom
 import './App.css'
 import useIsAuthenticated from './hooks/useIsAuthenticated'
 import useAuthenticateController from './hooks/useAuthenticateController'
 import routes from './configs/client/routes'
 import RouteModel from './models/other/route'
+import appTheme from './configs/client/theme/theme';
+import getGlobalStyle from './configs/client/theme/global-style';
+import Store from './models/other/store'
+import useStore from './store'
+import { ThemeMode } from './enums/theme'
 
 function App() {
     const isAuthenticated: boolean = useIsAuthenticated()
     useAuthenticateController()
 
+    const themeMode = useStore<ThemeMode>((store: Store) => store.themeMode)
+    const theme = useMemo(() => createTheme(appTheme(themeMode) as ThemeOptions), [themeMode])
+
     return (
-        <Routes>
-            {(isAuthenticated
-                ? routes
-                : routes.filter((route: RouteModel) => route.isPublic)
-            ).map(({ path, Layout, Cmp }: RouteModel) => (
-                <Route
-                    key={path}
-                    path={path}
-                    element={Layout ? <Layout Cmp={Cmp} /> : <Cmp />}
-                />
-            ))}
-        </Routes>
+        <>
+            <GlobalStyles styles={getGlobalStyle(themeMode)} />
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Routes>
+                    {(isAuthenticated
+                        ? routes
+                        : routes.filter((route: RouteModel) => route.isPublic)
+                    ).map(({ path, Layout, Cmp }: RouteModel) => (
+                        <Route
+                            key={path}
+                            path={path}
+                            element={Layout ? <Layout Cmp={Cmp} /> : <Cmp />}
+                        />
+                    ))}
+                </Routes>
+            </ThemeProvider>
+        </>
     )
 }
 
---
+++ src/configs/client/theme/global-style.ts
@@ -0,0 +1,37 @@
+import { ThemeMode } from "../../../enums/theme"
+
+const commonStyle = {
+    margin: 0,
+    padding: 0,
+    fontFamily: 'IRANYekan',
+    direction: 'rtl',
+    height: '100vh',
+    maxHeight: '100%',
+    width: '100%',
+}
+
+const globalLightStyle = {
+    html: {
+        height: '100vh',
+    },
+    body: {
+        ...commonStyle,
+        backgroundColor: 'white',
+        color: 'black',
+    },
+}
+
+const globalDarkStyle = {
+    html: {
+        height: '100vh',
+    },
+    body: {
+        ...commonStyle,
+        backgroundColor: '#040810',
+        color: '#DCE0E8',
+    },
+}
+
+const getGlobalStyle = (themeMode: ThemeMode) => (themeMode === ThemeMode.DARK ? globalDarkStyle : globalLightStyle)
+
+export default getGlobalStyle
